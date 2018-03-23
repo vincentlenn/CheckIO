62167f465004fb435adfcf62a9b0010e13a23edd
vincentlenn <31527882+vincentlenn@users.noreply.github.com>
2018-03-23 14:28:47 +0800

    Initial commit

---
+++ The Most Wanted Letter.py
@@ -0,0 +1,34 @@
+def checkio(text):
+    "The most frequent letter in lower case in a string."
+    count = {}
+    string = str.lower(text)
+    for letter in string:
+        if not str.isalpha(letter):
+            continue
+        else:
+            if count.__contains__(letter):
+                count[letter] = count[letter] + 1
+            else:
+                count[letter] = 1
+    print(count)
+    s = sorted(count.items(), key=lambda x:x[1], reverse=True)
+    print(s)
+    list = []
+    list.append(s[0][0])
+    for index in range(len(s)-1):
+        if s[0][1] > s[1][1]:
+            list.pop(0)
+            break
+        elif s[index][1] > s[index+1][1]:
+            break
+        else:
+            list.append(s[index+1][0])
+    if list:
+        list.sort()
+        return list[0]
+    else:
+        return s[0][0]
+
+data = raw_input("Please input string: ")
+re = checkio(data)
+print(re)
