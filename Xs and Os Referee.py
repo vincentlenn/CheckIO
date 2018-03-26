13bb72c3f16b6e07afdf786a12eb7e32874392df
vincentlenn <31527882+vincentlenn@users.noreply.github.com>
2018-03-26 17:35:00 +0800

    Xs and Os Referee

--- Xs and Os Referee.py
+++ Xs and Os Referee.py
@@ -57,7 +57,7 @@ if __name__ == '__main__':
     rows = result
     cols = map(''.join, zip(*rows))
     diags = map(''.join, zip(*[(r[i], r[2 - i]) for i, r in enumerate(rows)]))
-    lines = rows + list(cols) + list(diags)
+    lines = rows + list(cols) + list(diags)   #this is a sum including rows, cols and diags
 
     return 'X' if ('XXX' in lines) else 'O' if ('OOO' in lines) else 'D'
 """
